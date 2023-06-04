24623d8b3e06f19e094145bd361f0d15cc57f6a8
Utku Bilen Demir <84389167+UtkuBilenDemir@users.noreply.github.com>
2023-06-04 23:20:57 +0200

    Problem 10

--- eu_010.py
+++ eu_010.py
@@ -1,19 +1,12 @@
 # https://projecteuler.net/problem=10
-        
-p_list = [2]
-k = 3
-while len(p_list) < 2000000:
-    k_o = k
-    i = 0
-    while k_o>1 and p_list[i]<k/2:
-        if not k_o%p_list[i]:
-            k_o /= p_list[i]
-        else:
-            i += 1
-    if k_o == k:
-        p_list.insert(0, k)
-    k += 1
-    
-print(sum(p_list))
-print(len(p_list))
-    
+def eratosthenes(n):
+    sum, sieve = 0, [True] * (n+1)
+    for i in range(2, n+1):
+        if sieve[i]:
+            sum += i
+            for j in range(i**2, n+1, i):
+                sieve[j] = False
+    return sum
+
+
+print(eratosthenes(2000000))
